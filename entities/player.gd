e492b8dde4bbb9c779fdd49619a9c7ab3b4512a7
gabrielcavali <152876716+gabrielcavali222@users.noreply.github.com>
2025-03-13 21:43:39 -0300

    cenario parallax

--- entities/player.gd
+++ entities/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 50.0
+const SPEED = 55.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 900.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 55.0
+const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
-			if velocity.x != 0:
-				animation.play("walk")
-			else:
-				animation.play("idle")
+		if velocity.x != 0:
+			anim.play("walk")
+		else:
+			anim.play("idle")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animation.play("jump")
+		anim.play("jump")
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -27,12 +27,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	if velocity.x > 0:
-		animation.flip_h = false
-	elif velocity.x <0:
-		animation.flip_h = true
-	
+		anim.flip_h = false
+	elif velocity.x < 0:
+		anim.flip_h = true
+		
 
-	
-
+		
 	move_and_slide()
