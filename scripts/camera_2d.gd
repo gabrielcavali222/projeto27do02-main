1a3fab96bd02b406f89c0ab8fca1f575b13379b1
gabrielcavali <152876716+gabrielcavali222@users.noreply.github.com>
2025-04-17 20:23:04 -0300

    finaldojogo

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,11 +1,13 @@
 extends Camera2D
 
-
-var player: Node2D
+var player: Node
 
 func _ready() -> void:
-	player = get_tree().get_first_node_in_group("Player")
+	var players = get_tree().get_nodes_in_group("Player")
+	if players.size() > 0:
+		player = players[0]
+	else:
+		push_error("player not found")
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	self.position = player.position
--- scripts/player.gd
+++ scripts/player.gd
@@ -15,7 +15,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	else:
 		jump_count = 0
-		
 		if velocity.x != 0:
 			anim.play("walk")
 		else:
@@ -23,9 +22,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
-		anim.play("jump")
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,5 +40,31 @@ func _physics_process(delta: float) -> void:
 
 		
 	move_and_slide()
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+	jump_count += 1 
+	anim.play("jump")
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("DeathZone"):
+		call_deferred("reload_scene")
+	elif area.is_in_group("LevelEnd"):
+		var next_level = area.next_level
+		if next_level:
+			call_deferred("load_scene", next_level)
+		else:
+			push_error("Próxima fase não definida em EndLevel")
+	elif area.is_in_group("Enemies"):
+		if velocity.y > 0: # o player matou o inimigo
+			area.take_damage() # deleta o inimigo
+			jump()
+		else:
+			reload_scene()
+			
+
+func reload_scene():
+	get_tree().reload_current_scene()
 	
-	
+func load_scene(scene_name: String):
+	get_tree().change_scene_to_file("res://scenes/" + scene_name + ".tscn")
